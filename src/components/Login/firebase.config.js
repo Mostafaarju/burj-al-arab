1eb3c448d034902b262ee606c04ce06dfad85e70
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-05-06 21:56:57 +0200

    My Another Project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3vrfbOMkFnnHeVttBGUCFNrDND6HOJCQ",
+  authDomain: "burj-al-arab-86d23.firebaseapp.com",
+  projectId: "burj-al-arab-86d23",
+  storageBucket: "burj-al-arab-86d23.appspot.com",
+  messagingSenderId: "70264296217",
+  appId: "1:70264296217:web:22f9603d0dc3da4032ab88",
+};
+export default firebaseConfig;
